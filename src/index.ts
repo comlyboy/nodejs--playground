4039f832d01ff587c84cfda81425a53c1b9fb763
Cornelius Okeke <okekecornelius@gmail.com>
2024-08-30 23:15:05 +0100

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,47 @@
 import knex from "knex";
 
-try {
-	const sqlBuilder = knex({ client: 'pg' });
+// export class PostgresSqlBulder {
+// 	private readonly sqlBuilder = knex({ client: 'pg' });
 
-	const sqlCreateQuery = sqlBuilder('user').where('id', 1).update({ name: 'Jane Doe' }).toSQL();
-	const sqlUpdateQuery = sqlBuilder('user').where('id', 1).update({ name: 'Jane Doe' }).toSQL();
-	const sqlQuery2 = sqlBuilder('users').where('id', 1).select().toSQL().sql;
+// 	query() {
+// 		const createQueryKnex = this.sqlBuilder('user').insert({ name: 'Jane Doe' }).toString();
+// 		// const sqlUpdateQuery = this.sqlBuilder('user').where('id', 1).update({ name: 'Jane Doe' }).toString();
+// 		// const sqlQuery2 = this.sqlBuilder('users').select().where('id', 1).toString();
 
 
-	console.log('sqlCreateQuery =', sqlCreateQuery);
-	console.log('sqlUpdateQuery =', sqlUpdateQuery);
-	console.log('sqlQuery2 =', sqlQuery2);
+// 		console.log('createQueryKnex =', createQueryKnex);
 
-} catch (error) {
-	console.log('error =>', error.message);
-}
+// 	}
+
+// }
+
+
+// try {
+
+
+
+// } catch (error) {
+// 	console.log('error =>', error.message);
+// }
+
+// export function sqlBuilder() {
+// 	const sqlBuilder = knex({ client: 'pg' });
+
+// 	const createQueryKnex = sqlBuilder('user').insert({ name: 'Jane Doe' }).toString();
+// 	// const sqlUpdateQuery = this.sqlBuilder('user').where('id', 1).update({ name: 'Jane Doe' }).toString();
+// 	// const sqlQuery2 = this.sqlBuilder('users').select().where('id', 1).toString();
+
+
+// 	console.log('createQueryKnex =', createQueryKnex);
+
+// }
+
+
+const sqlBuilder = knex({ client: 'pg' });
+
+const createQueryKnex = sqlBuilder('user').insert({ name: 'Jane Doe' }).toString();
+// const sqlUpdateQuery = this.sqlBuilder('user').where('id', 1).update({ name: 'Jane Doe' }).toString();
+// const sqlQuery2 = this.sqlBuilder('users').select().where('id', 1).toString();
+
+
+console.log('createQueryKnex =', createQueryKnex);
