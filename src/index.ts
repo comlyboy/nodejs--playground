48c712823635ce6db85017ccfef9f264ecbc0b24
Cornelius Okeke <okekecornelius@gmail.com>
2024-09-10 21:08:42 +0100

    fixes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import knex from "knex";
+import axios from "axios";
+import jsPDF from "jspdf";
+// import knex from "knex";
 
 // export class PostgresSqlBulder {
 // 	private readonly sqlBuilder = knex({ client: 'pg' });
@@ -24,24 +26,34 @@ import knex from "knex";
 // 	console.log('error =>', error.message);
 // }
 
-// export function sqlBuilder() {
-// 	const sqlBuilder = knex({ client: 'pg' });
-
-// 	const createQueryKnex = sqlBuilder('user').insert({ name: 'Jane Doe' }).toString();
-// 	// const sqlUpdateQuery = this.sqlBuilder('user').where('id', 1).update({ name: 'Jane Doe' }).toString();
-// 	// const sqlQuery2 = this.sqlBuilder('users').select().where('id', 1).toString();
+// type Lower<TSchema = any> = Lowercase<keyof TSchema & string>;
 
 
-// 	console.log('createQueryKnex =', createQueryKnex);
-
+// export function sqlBuilder<TEntitySchema extends Record<string, any> = any>(tableName: string) {
+// 	if (!tableName) throw new Error('Table name is needed');
+// 	return knex<Lower<Partial<TEntitySchema>>>({ client: 'pg' })(tableName);
 // }
 
-
-const sqlBuilder = knex({ client: 'pg' });
-
-const createQueryKnex = sqlBuilder('user').insert({ name: 'Jane Doe' }).toString();
-// const sqlUpdateQuery = this.sqlBuilder('user').where('id', 1).update({ name: 'Jane Doe' }).toString();
-// const sqlQuery2 = this.sqlBuilder('users').select().where('id', 1).toString();
+// const createQueryKnex = sqlBuilder('user').insert({  }).toString();
+// const sqlUpdateQuery = sqlBuilder('user').where('id', 1).update({ name: 'Jane Doe' }).toString();
+// // const sqlQuery2 = this.sqlBuilder('users').select().where('id', 1).toString();
 
 
-console.log('createQueryKnex =', createQueryKnex);
+// console.log('sqlUpdateQuery =', sqlUpdateQuery);
+// console.log('createQueryKnex =', createQueryKnex);
+
+async function generatePDFfromURL(url: string, outputPath: string) {
+	try {
+		const response = await axios.get(url);
+		const textContent = response.data;
+		const doc = new jsPDF();
+		doc.text(textContent, 10, 10);
+		doc.save(outputPath);
+		console.log('PDF generated successfully');
+	} catch (error) {
+		console.error('Error fetching URL:', error);
+	}
+}
+
+// Usage
+generatePDFfromURL('https://google.com', 'google.pdf');
